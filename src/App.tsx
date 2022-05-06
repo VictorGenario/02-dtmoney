54620bc5e66647a4f12496d016ce51800b1bb1d6
VictorGenario <victordamatagenario@gmail.com>
2022-05-05 21:38:42 -0300

    criando o projeto

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { GlobalStyle } from "./styles/global";
+
+export function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      <GlobalStyle />
+    </div>
+  );
+}
+
+
